7c22bbb2d2e23d50afb652aa85f0a67a25f2bbfe
Alex Alikhani <alikhanialex@gmail.com>
2022-04-23 08:56:56 +0100

    Reading the array of items in the database by going to /read

--- server/index.js
+++ server/index.js
@@ -8,10 +8,20 @@ const ItemModel = require("./models/Items");
 //Database Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-app.get("/insert", async (req, res) => {
+app.get("/insert", async (req, res) => { // Going to http://localhost:5000/insert will manually insert this data into teh database
   const item = new ItemModel({ name: "Blueberries", price: 3 });
   await item.save();
-  res.send('Inserted Data!')
+  res.send("Inserted Data!");
+});
+
+app.get("/read", async (req, res) => { // Going to http://localhost:5000/read will display an array of the items
+  ItemModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.listen(PORT, () => {
