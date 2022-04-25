c7ee5c8976236471c3077c7312e613e5e14a791f
Alex Alikhani <alikhanialex@gmail.com>
2022-04-25 20:29:09 +0100

    Setting state in the inputs for product and price and creating basic onClick function to add product with alert

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [product, setProduct] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const addProduct = () => {
+    alert(product + " " + price)
+  }
+
   return (
     <div className="App">
       <div className="inputs">
-      <input type="text" />
-      <input type="number" />
+        <input
+          type="text"
+          placeholder="Product"
+          onChange={(event) => {
+            setProduct(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="price"
+          onChange={(event) => {
+            setPrice(event.target.value);
+          }}
+        />
+
+        <button onClick={addProduct}>Add Product</button>
       </div>
     </div>
   );
